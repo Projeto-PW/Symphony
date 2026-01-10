6bedf559f574ffbc7abaf44cf147f6ef05f19d4b
Lucas <lcorte110210@gmail.com>
2026-01-09 21:01:19 -0300

    Programando botões de voltar e avançar música

--- src/js/music.js
+++ src/js/music.js
@@ -3,28 +3,63 @@ const songs = [
         nome: "Keys to the Kingdom",
         artista: "Linkin Park",
         capa: "./src/assets/capaTeste.webp",
-        audio: new Audio("./src/assets/audio/Teste.mp3")
+        audio: "./src/assets/audio/Teste.mp3"
+    },
+    {
+        nome: "Come As You Are",
+        artista: "Nirvana",
+        capa: "./src/assets/capaTeste2.webp",
+        audio: "./src/assets/audio/Teste2.mp3"
     }
 ];
 
-const audio = songs[0].audio;
-audio.load();
+var songIndex = 0;
+let audio = new Audio();
+
 const albumImg = document.getElementById("albumImg");
 const songTitle = document.querySelector(".info-text").querySelector("h2");
 const songArtist = document.querySelector(".info-text").querySelector("h3");
 const durationText = document.getElementById("tempoTotal");
 const currentTimeText = document.getElementById("tempoAtual");
-albumImg.style.backgroundImage = `url(${songs[0].capa})`;
-songTitle.innerText = songs[0].nome;
-songArtist.innerText = songs[0].artista;
-currentTimeText.innerText = "0:00";
 
-audio.addEventListener("loadedmetadata", () => {
-    const durationTime = audio.duration;
-    const minutes = Math.floor(durationTime / 60);
-    const seconds = Math.floor(durationTime - (minutes * 60));
-    durationText.innerText = `${minutes}:${String(seconds).padStart(2, "0")}`;
+function loadSong(song) {
+    audio.pause();
+    audio.currentTime = 0;
+    audio.src = songs[song].audio;
+    audio.load();
+    albumImg.style.backgroundImage = `url(${songs[song].capa})`;
+    songTitle.innerText = songs[song].nome;
+    songArtist.innerText = songs[song].artista;
+    currentTimeText.innerText = "0:00";
+
+    audio.addEventListener("loadedmetadata", () => {
+        const durationTime = audio.duration;
+        const minutes = Math.floor(durationTime / 60);
+        const seconds = Math.floor(durationTime - (minutes * 60));
+        durationText.innerText = `${minutes}:${String(seconds).padStart(2, "0")}`;
+    });
+
+    audio.addEventListener("ended", () => {
+        btnPlay.querySelector("img").src = "./src/assets/play.svg";
+        clearInterval(updateTime);
+    });
+}
+
+window.addEventListener("load", () => {
+    loadSong(songIndex);    
 });
+
+function selSong(index) {
+    const nextIndex = songIndex + index;
+
+    if(nextIndex < 0 || nextIndex >= songs.length) {
+        alert(`Não há mais músicas ${index === 1 ? "depois" : "antes"} dessa`);
+        return;  
+    }
+    songIndex = nextIndex;
+    loadSong(songIndex);
+    playSong();
+}
 
 const timeline = document.querySelector(".timeline");
 const timeball = document.querySelector(".time-ball");
@@ -73,8 +108,4 @@ timeline.addEventListener("click", (e) => {
     const duration = audio.duration;
     audio.currentTime = (duration * distance) / timelineWidth;
     updateCurrentTime();
-})
-
-audio.addEventListener("ended", () => {
-    btnPlay.querySelector("img").src = "./src/assets/play.svg";
 });
