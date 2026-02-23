9b4895da9884e1c314f0982a815fce38fb57abc6
Lucas <lcorte110210@gmail.com>
2026-02-23 15:55:52 -0300

    Programando scroll infinito da lista de músicas

--- src/js/musicList.js
+++ src/js/musicList.js
@@ -1,5 +1,7 @@
 const list = document.getElementById('musicList');
 const listItems = document.querySelectorAll('.song');
+const step = 75;
+let isLooping = false; // Trava de segurança para impedir repetição infinita do loop
 
 function updateList() {
     const listRect = list.getBoundingClientRect();
@@ -15,5 +17,49 @@ function updateList() {
     });
 }
 
+function scrollList(d) {
+    list.scrollBy({
+        top: d * step,
+        behavior: "smooth"
+    });
+}
+
+function loopListIfNeeded() {
+    const errorMargin = 10; // Margem de erro para o scroll
+    if(list.scrollTop + list.clientHeight >= list.scrollHeight - errorMargin) { // Detecta se chegou no final da lista
+        isLooping = true;
+        const firstItem = list.firstElementChild;
+        list.appendChild(firstItem); // Move o primeiro item ao final da lista
+        list.scrollTop -= step; // Subtrai um scroll, para continuar no mesmo item
+        isLooping = false;
+    }
+    else if(list.scrollTop <= errorMargin) {
+        isLooping = true;
+        const lastItem = list.lastElementChild;
+        list.prepend(lastItem);
+        list.scrollTop += step;
+        isLooping = false;
+    }
+}
+
 window.addEventListener("load", updateList);
-list.addEventListener("scroll", updateList);
+list.addEventListener("wheel", (e) => {
+    e.preventDefault();
+    scrollList(e.deltaY);
+});
+list.addEventListener("scroll", () => {
+    updateList();
+    if(!isLooping) {
+        loopListIfNeeded();
+    }
+});
+window.addEventListener("keydown", (e) => {
+    if(e.key === "ArrowUp") {
+        e.preventDefault();
+        scrollList(-1);
+    }
+    if(e.key === "ArrowDown") {
+        e.preventDefault();
+        scrollList(1);
+    }
+});
